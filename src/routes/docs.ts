75e4975562edbd24db734b147710a4a7afdccee3
Dho99 <ridhoawwaludin@gmail.com>
2026-04-29 22:32:17 +0700

    swagger serve files

--- src/routes/docs.ts
+++ src/routes/docs.ts
@@ -5,23 +5,20 @@ import swaggerSpec from '../docs/swagger';
 const docsRouter = Router();
 
 // Serve Swagger UI static files first
-docsRouter.use(swaggerUi.serve);
+docsRouter.use('/', swaggerUi.serveFiles(swaggerSpec));
 
-// Setup Swagger UI HTML
+// setup UI
 docsRouter.get(
   '/',
   swaggerUi.setup(swaggerSpec, {
     explorer: true,
-    swaggerOptions: {
-      url: '/api-docs/swagger.json',
-    },
   }),
 );
 
 // Serve raw OpenAPI spec as JSON
-docsRouter.get('/swagger.json', (_req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(swaggerSpec);
-});
+// docsRouter.get('/swagger.json', (_req, res) => {
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(swaggerSpec);
+// });
 
 export default docsRouter;
