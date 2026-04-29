bec935bb93725f1fc303628a192c293adca68bc9
Dho99 <ridhoawwaludin@gmail.com>
2026-04-29 22:39:31 +0700

    swagger cdn

--- src/routes/docs.ts
+++ src/routes/docs.ts
@@ -8,17 +8,42 @@ const docsRouter = Router();
 docsRouter.use('/', swaggerUi.serveFiles(swaggerSpec));
 
 // setup UI
-docsRouter.get(
-  '/',
-  swaggerUi.setup(swaggerSpec, {
-    explorer: true,
-  }),
-);
+docsRouter.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>API Docs</title>
+        <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+      </head>
+      <body>
+        <div id="swagger-ui"></div>
+
+        <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+        <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js"></script>
+
+        <script>
+          window.onload = function () {
+            window.ui = SwaggerUIBundle({
+              spec: ${JSON.stringify(swaggerSpec)},
+              dom_id: '#swagger-ui',
+              presets: [
+                SwaggerUIBundle.presets.apis,
+                SwaggerUIStandalonePreset
+              ],
+              layout: "StandaloneLayout"
+            });
+          };
+        </script>
+      </body>
+    </html>
+  `);
+});
 
 // Serve raw OpenAPI spec as JSON
-// docsRouter.get('/swagger.json', (_req, res) => {
-//   res.setHeader('Content-Type', 'application/json');
-//   res.send(swaggerSpec);
-// });
+docsRouter.get('/swagger.json', (_req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
 export default docsRouter;
